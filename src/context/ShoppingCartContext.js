6128eee6170da3c9efe900c16754bd656d768fae
Herman Morales Ara <hmoralesara@gmail.com>
2022-08-09 22:25:26 -0500

    addssss

--- src/context/ShoppingCartContext.js
+++ src/context/ShoppingCartContext.js
@@ -32,6 +32,19 @@ export const ShoppingCartProvider = ({ children }) => {
     return movie;
   }
 
+  function upOne(id) {
+    const movie = movieIsInCart(id);
+    if (movie === undefined) return;
+    if (movie.quantity >= 10) return;
+    const index = items.findIndex(
+      (item) => item.movie.imdbID === id && item.user_id === user.id
+    );
+    movie.quantity++;
+    items[index] = movie;
+    setItems([...items]);
+    saveInLocalStorage(items);
+  }
+
   return (
     <ShoppingCartContext.Provider value={{ items, saveInCart, movieIsInCart }}>
       {children}
