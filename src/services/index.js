1e10e243169b62339a11717683b346e1af802495
Herman Morales Ara <hmoralesara@gmail.com>
2022-08-09 20:32:40 -0500

    subiendo .envvv

--- src/services/index.js
+++ src/services/index.js
@@ -1,19 +1,20 @@
 import Config from "../config.js";
 
 const searchByText = async (searchText) => {
-	try {
-		const url =
-			Config.api.url + "?s=" + searchText + "&apikey=" + Config.api.key;
-		const response = await fetch(url);
-		const data = await response.json();
-		return data;
-	} catch (error) {
-		return error;
-	}
+  try {
+    const url =
+      Config.api.url + "?s=" + searchText + "&apikey=" + Config.api.key;
+    console.log("URLLL", url);
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 const Services = {
-	searchByText,
+  searchByText,
 };
 
 export default Services;
