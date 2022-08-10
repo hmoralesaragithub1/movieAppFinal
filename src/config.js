c2d1f9b6cfb40ee0b01018365c9f2b5120680e77
Herman Morales Ara <hmoralesara@gmail.com>
2022-08-09 20:12:25 -0500

    subiendo .env

--- src/config.js
+++ src/config.js
@@ -1,38 +1,41 @@
+const ODMB_API_KEY = process.env.REACT_APP_OMDB_API_KEY || "";
+const ODMB_BASE_URL = process.env.REACT_APP_OMDB_BASE_URL || "";
+
 const api = {
-	url: "https://www.omdbapi.com",
-	key: "e50b7182",
+  apiUrl: ODMB_BASE_URL,
+  apiKey: ODMB_API_KEY,
 };
 
 export const authUsers = [
-	{
-		id: "1001",
-		username: "admin",
-		name: "Admin",
-		pass: "1234",
-	},
-	{
-		id: "1002",
-		username: "bruno",
-		name: "Bruno Díaz",
-		pass: "1234",
-	},
-	{
-		id: "1003",
-		username: "linder",
-		name: "Linder Hassinger",
-		pass: "1234",
-	},
-	{
-		id: "1004",
-		username: "andres",
-		name: "Andres Taboada",
-		pass: "1234",
-	},
+  {
+    id: "1001",
+    username: "admin",
+    name: "Admin",
+    pass: "1234",
+  },
+  {
+    id: "1002",
+    username: "bruno",
+    name: "Bruno Díaz",
+    pass: "1234",
+  },
+  {
+    id: "1003",
+    username: "linder",
+    name: "Linder Hassinger",
+    pass: "1234",
+  },
+  {
+    id: "1004",
+    username: "andres",
+    name: "Andres Taboada",
+    pass: "1234",
+  },
 ];
 
 const Config = {
-	authUsers,
-	api,
+  authUsers,
+  api,
 };
 
 export default Config;
