c2dd02a71a6c478f6c11f27d08945d739bb3d932
Herman Morales Ara <hmoralesara@gmail.com>
2022-08-09 20:23:24 -0500

    ojalahh

--- src/config.js
+++ src/config.js
@@ -1,6 +1,12 @@
 const ODMB_API_KEY = process.env.REACT_APP_OMDB_API_KEY || "";
 const ODMB_BASE_URL = process.env.REACT_APP_OMDB_BASE_URL || "";
 
+console.log(
+  "apikey",
+  process.env.REACT_APP_OMDB_API_KEY,
+  process.env.REACT_APP_OMDB_BASE_URL
+);
+
 const api = {
   apiUrl: ODMB_BASE_URL,
   apiKey: ODMB_API_KEY,
