1d77dc7954728d131feb76d36c6a68a66069099c
Herman Morales Ara <hmoralesara@gmail.com>
2022-08-09 22:02:12 -0500

    add

--- src/components/CardMovie/index.js
+++ src/components/CardMovie/index.js
@@ -1,129 +1,152 @@
 import { useContext, useState, useEffect } from "react";
 import { MovieFavoriteContext, ShoppingCartContext } from "../../context";
 import {
-	Card,
-	CardContent,
-	CardMedia,
-	Grid,
-	Typography,
-	Chip,
-	Rating,
-	Stack,
-	Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  Chip,
+  Rating,
+  Stack,
+  Button,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
+import React from "react";
+
+const CartButton = ({ movie }) => {
+  const { saveInCart, movieIsInCart } = useContext(ShoppingCartContext);
+  const movieInCart = movieIsInCart(movie.imdbID);
+  return (
+    <>
+      {movieInCart === undefined ? (
+        <Button
+          variant="contained"
+          size="small"
+          onClick={() => saveInCart(movie)}
+        >
+          Add to cart
+        </Button>
+      ) : (
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Button>-</Button>
+          <Typography
+            sx={{
+              width: 40,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              fontWeight: "bold",
+            }}
+          >
+            {movieInCart.quantity}
+          </Typography>
+          <Button>+</Button>
+        </ButtonGroup>
+      )}
+    </>
+  );
+};
+
 const CardMovie = ({ movie }) => {
-	const { addToFavorite, isIncludeInFavorites, removeFavorite } =
-		useContext(MovieFavoriteContext);
+  const { addToFavorite, isIncludeInFavorites, removeFavorite } =
+    useContext(MovieFavoriteContext);
 
-	const { saveInCart } = useContext(ShoppingCartContext);
+  const { saveInCart } = useContext(ShoppingCartContext);
 
-	// sirve para saber si debemos pintar el corazon
-	const [value, setValue] = useState(0);
+  // sirve para saber si debemos pintar el corazon
+  const [value, setValue] = useState(0);
 
-	const StyledRating = styled(Rating)({
-		"& .MuiRating-iconFilled": {
-			color: "#ff6d75",
-		},
-		"& .MuiRating-iconHover": {
-			color: "#ff3d47",
-		},
-	});
+  const StyledRating = styled(Rating)({
+    "& .MuiRating-iconFilled": {
+      color: "#ff6d75",
+    },
+    "& .MuiRating-iconHover": {
+      color: "#ff3d47",
+    },
+  });
 
-	const addToCart = () => {
-		saveInCart(movie);
-	};
+  const addToCart = () => {
+    saveInCart(movie);
+  };
 
-	const handleChangeFavorite = (event, newValue) => {
-		if (newValue === 1) {
-			addToFavorite(movie);
-		} else {
-			removeFavorite(movie.imdbID);
-		}
-		setValue(newValue);
-	};
+  const handleChangeFavorite = (event, newValue) => {
+    if (newValue === 1) {
+      addToFavorite(movie);
+    } else {
+      removeFavorite(movie.imdbID);
+    }
+    setValue(newValue);
+  };
 
-	useEffect(() => {
-		const pintado = isIncludeInFavorites(movie.imdbID);
-		setValue(pintado);
-	}, [value]);
+  useEffect(() => {
+    const pintado = isIncludeInFavorites(movie.imdbID);
+    setValue(pintado);
+  }, [value]);
 
-	return (
-		<Grid item xs={12} md={6} my={3}>
-			<Card
-				sx={{
-					height: 440,
-				}}
-			>
-				<CardMedia
-					component="img"
-					height="240"
-					sx={{
-						objectPosition: "top",
-					}}
-					image={movie.Poster}
-					alt="green iguana"
-				/>
-				<CardContent
-					sx={{
-						cursor: "pointer",
-					}}
-				>
-					<Grid container spacing={3}>
-						<Grid item xs={12}>
-							<Typography
-								variant="h6"
-								sx={{ fontWeight: "bold" }}
-							>
-								{movie.Title}
-							</Typography>
-							<Typography variant="body1">
-								{movie.Year}
-							</Typography>
-							<Typography variant="h6" color="red">
-								$ {movie.Price}
-							</Typography>
-							<Chip
-								label={movie.Type}
-								color="success"
-								size="small"
-								variant="outlined"
-							/>
-						</Grid>
-						<Grid item xs={12}>
-							<Stack
-								direction="row"
-								justifyContent="space-between"
-							>
-								<StyledRating
-									max={1}
-									value={value}
-									icon={<FavoriteIcon fontSize="inherit" />}
-									emptyIcon={
-										<FavoriteBorderIcon fontSize="inherit" />
-									}
-									onChange={(event, newValue) =>
-										handleChangeFavorite(event, newValue)
-									}
-								/>
-								<Button
-									variant="contained"
-									size="small"
-									onClick={addToCart}
-								>
-									Add to cart
-								</Button>
-								{/* El corazon se pinta si es que value = 1 */}
-							</Stack>
-						</Grid>
-					</Grid>
-				</CardContent>
-			</Card>
-		</Grid>
-	);
+  return (
+    <Grid item xs={12} md={6} my={3}>
+      <Card
+        sx={{
+          height: 440,
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="240"
+          sx={{
+            objectPosition: "top",
+          }}
+          image={movie.Poster}
+          alt="green iguana"
+        />
+        <CardContent
+          sx={{
+            cursor: "pointer",
+          }}
+        >
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                {movie.Title}
+              </Typography>
+              <Typography variant="body1">{movie.Year}</Typography>
+              <Typography variant="h6" color="red">
+                $ {movie.Price}
+              </Typography>
+              <Chip
+                label={movie.Type}
+                color="success"
+                size="small"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Stack direction="row" justifyContent="space-between">
+                <StyledRating
+                  max={1}
+                  value={value}
+                  icon={<FavoriteIcon fontSize="inherit" />}
+                  emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+                  onChange={(event, newValue) =>
+                    handleChangeFavorite(event, newValue)
+                  }
+                />
+                <CartButton movie={movie} />
+                {/* El corazon se pinta si es que value = 1 */}
+              </Stack>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 };
 
 export default CardMovie;
--- src/context/ShoppingCartContext.js
+++ src/context/ShoppingCartContext.js
@@ -4,30 +4,37 @@ import { AuthContext } from "./AuthContext";
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
-	const { user } = useContext(AuthContext);
-	const [items, setItems] = useState(
-		JSON.parse(localStorage.getItem("movieapp.shoppingcart")) ?? []
-	);
+  const { user } = useContext(AuthContext);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("movieapp.shoppingcart")) ?? []
+  );
 
-	function saveInCart(movie) {
-		const object = {
-			movie,
-			user_id: user.id,
-			quantity: 1,
-		};
+  function saveInCart(movie) {
+    const object = {
+      movie,
+      user_id: user.id,
+      quantity: 1,
+    };
 
-		items[items.length] = object;
-		setItems([...items]);
-		saveInLocalStorage(items);
-	}
+    items[items.length] = object;
+    setItems([...items]);
+    saveInLocalStorage(items);
+  }
 
-	function saveInLocalStorage(items) {
-		localStorage.setItem("movieapp.shoppingcart", JSON.stringify(items));
-	}
+  function saveInLocalStorage(items) {
+    localStorage.setItem("movieapp.shoppingcart", JSON.stringify(items));
+  }
 
-	return (
-		<ShoppingCartContext.Provider value={{ items, saveInCart }}>
-			{children}
-		</ShoppingCartContext.Provider>
-	);
+  function movieIsInCart(id) {
+    const movie = items.find(
+      (item) => item.movie.imdbID === id && item.movie.user_id === user.id
+    );
+    return movie;
+  }
+
+  return (
+    <ShoppingCartContext.Provider value={{ items, saveInCart, movieIsInCart }}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
 };
